51448344cc4d67a5c4e4d8dcd2db10539f7c2d38
Sara <sarazebdeh73@gmail.com>
2024-07-26 12:54:54 +0300

    go to the details page

--- src/components/Topic/Topic.jsx
+++ src/components/Topic/Topic.jsx
@@ -1,24 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import Stars from "../Stars/Stars";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./topic.css";
 
 const Topic = (props) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/topic/${props.id}`);
+  };
+
   return (
-    <Link to={`/topic/${props.id}`} key={props.id}>
-      <div className="topic">
-        <div className="topic-img">
-          <img src={`/${props.image}`} alt={props.topic} />
-        </div>
-        <div className="information-for-topic">
-          <p className="topicCategory">{props.category}</p>
-          <h3>{props.topic}</h3>
-          <Stars rating={props.rating} />
-          <p className="auther-p">Author: {props.name}</p>
-        </div>
+    // <Link to={`/topic/${props.id}`} key={props.id}>
+    <div className="topic" onClick={handleClick}>
+      <div className="topic-img">
+        <img src={`/${props.image}`} alt={props.topic} />
       </div>
-    </Link>
+      <div className="information-for-topic">
+        <p className="topicCategory">{props.category}</p>
+        <h3>{props.topic}</h3>
+        <Stars rating={props.rating} />
+        <p className="auther-p">Author: {props.name}</p>
+      </div>
+    </div>
+    // </Link>
   );
 };
 
